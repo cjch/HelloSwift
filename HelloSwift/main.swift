b4f42c699b1ef58ad5eb9c78fda1519cdbcacf35
cjch <350765427@qq.com>
2016-01-21 17:41:15 +0800

    collection closure

--- HelloSwift/main.swift
+++ HelloSwift/main.swift
@@ -10,15 +10,16 @@ import Foundation
 
 print("Hello, World!")
 
-//MARK: type
+//MARK: - type
 var n = Int(3.14)
-print(n)
+var n1 = 3.141 as Int
+print(n, n1)
 
 typealias Volume = Int
 var vol:Volume = 100
 print(vol)
 
-//MARK: operator
+//MARK: - operator
 print("close - close")
 for num in 1...3 {
     print(num)
@@ -33,3 +34,69 @@ var str: String?
 print(str ?? "nil variable")
 str = "non empty variable"
 print(str ?? "nil variable")
+
+//MARK: - collection
+let sList = ["ab", "ab1"]
+var sList1: [String] = ["ab2", "ab3"]
+var sList2: Array<String> = ["ab4", "ab5"]
+print(sList, sList1, sList2)
+
+let aSet: Set<Character> = ["c", "h", "a"]
+let aSet1: Set = ["cc", "hh", "aa"]
+print(aSet, aSet1)
+
+let aDict: Dictionary<Int, String> = [1: "aaa"]
+var aDict1: [Int: String] = [2: "bbb"]
+var aDict2 = [2: "bbb"]
+print(aDict, aDict1, aDict2)
+
+//MARK: - switch, where
+let point = (4, 4)
+switch point {
+case (0, 0):
+    print("\(point) is at origin")
+case (_, 0):
+    print("(\(point.0), 0) is at x-axis")
+case let (x, y) where x == y:
+    print("\(point) is on the line x == y")
+case (0, let y):
+    print("(0, \(y)) is at y-axis")
+case (-2...2, -2...2):
+    print("\(point) is inside box")
+default:
+    print("\(point) is outside box")
+}
+
+let wx: Int? = 5
+let wy: Int? = -5
+if let x = wx, y = wy where x == -y {
+    print("(\(x), \(y)) is on the line x == -y")
+}
+
+//MARK: - func
+func swipInts(inout a: Int, inout b: Int) {
+    let temp = a;
+    a = b;
+    b = temp;
+}
+
+var ia = 5, ib = 6
+print("before swap: ", ia, ib)
+swipInts(&ia, b: &ib)
+print("after swap: ", ia, ib)
+
+//MARK: - closure
+func compareStr(s1: String, s2: String) -> Bool {
+    return s1 > s2
+}
+
+let originList = ["I", "have", "an", "apple", "!"]
+let rst1 = originList.sort(compareStr)
+let rst2 = originList.sort(>)
+print("function sort: ", rst1, rst2, separator: "\n")
+
+//以下几种闭包写法是一样的
+let rst3 = originList.sort({ (s1: String, s2: String) -> Bool in return s1 > s2 })
+let rst4 = originList.sort({ s1, s2 in return s1 > s2 })
+let rst5 = originList.sort({ $0 > $1 })
+print("closure sort: ", rst3, rst4, rst5, separator: "\n")
--- README.md
+++ README.md
@@ -9,8 +9,37 @@
 类型转换: float-->int: Int(3.14)  
 类型别名：typealias Volume = Int  
 
----
+--
 区间运算符：全闭区间 a...b, 左闭右开 a..<b  
 空合运算符：??，对可选类型进行解析，如果可选类型值为nil，则返回默认值。a ?? b 相当于 a != nil ? a! : b  
 
-字符串是值类型
+字符串(String), 集合（Array, Set, Dcitionary）是值类型  
+
+do-while --> repeat-while
+switch 不会自动贯穿，贯穿需使用fallthrough关键字。匹配时，多个值可以写在一行，也可以使用区间。针对tuple类型，可以使用 _ 匹配任意值，同时在case中还可以使用值绑定。
+
+where语句可用于switch和if语句中判断额外的条件
+
+guard和if类似，不同支持在于其必须有else分支，同时else分支必须退出guard出现的代码段
+
+函数参数默认为常量。若需要函数参数为变量，则添加var关键字，如果使函数参数为输入输出变量，需要使用inout关键字，调用时在参数前加&符号
+
+--
+闭包：闭包是引用类型。 闭包参数和返回值类型可隐式推断时，可省略；单表达式闭包隐式返回（可以不写return）；参数名称也可缩写，用$[0...]替换。  
+尾随闭包：如果闭包是函数的最后一个参数时，则可将闭包写在函数之后，如果函数只有1个参数，还可省略（） 
+
+``` (swift)
+func tailClosure(closure: () -> Void )
+
+// non trailing closure
+tailClosure( {
+// closure body
+})
+
+// trailing closure
+tailClosure {
+// closure body
+}
+``` 
+非逃逸闭包: 函数参数使用@noescape修饰  
+自动闭包：@autoclosure, 暗含@noescape特性，如果闭包可逃逸，则应使用@autoclosure(escaping)
